17dafe065035b3a651e5bec54729cab510a3c0f6
Pablo Orviz <orviz@ifca.unican.es>
2023-07-14 10:08:41 +0200

    Added curl for creating the assessment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,29 @@
 #!/bin/sh -l
 
-repo=$1
-branch=$2
+REPO=$1
+BRANCH=$2
 
-if [ -n "$branch" ]; then
-    branch_msg='using default branch'
+PAYLOAD='{
+  "repo_code": {
+    "repo": "'$REPO'",
+    "branch": "'$BRANCH'"
+  },
+  "repo_docs": {
+    "repo": "",
+    "branch": ""
+  }
+}'
+
+API_ENDPOINT=https://api-staging.sqaaas.eosc-synergy.eu/v1
+ 
+if [ -n "$BRANCH" ]; then
+    BRANCH_MSG='using default branch'
 else
-    branch_msg="branch: $branch"
+    BRANCH_MSG="branch: $BRANCH"
 fi 
 
-echo "Triggering SQAaaS assessment for repository $repo ($branch_msg)"
+echo "Triggering SQAaaS assessment for repository $REPO ($BRANCH_MSG)"
+curl -X POST $API_ENDPOINT/pipeline/assessment --header '"Content-Type: application/json"' -d "$PAYLOAD"
+# --header "Authorization: Bearer ${TOKEN}"
 
-echo "report={}" >> "$GITHUB_OUTPUT"
+# echo "report={}" >> "$GITHUB_OUTPUT"
