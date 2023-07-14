485b0fb8aa49db4f82216ec52215552a18485446
Pablo Orviz <orviz@ifca.unican.es>
2023-07-14 10:34:42 +0200

    Use 'jq' to create the JSON payload

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,27 +3,24 @@
 REPO=$1
 BRANCH=$2
 
-PAYLOAD='{
-  "repo_code": {
-    "repo": "'$REPO'",
-    "branch": "'$BRANCH'"
-  },
-  "repo_docs": {
-    "repo": "",
-    "branch": ""
-  }
-}'
+CODE_PAYLOAD=$(jq --null-input \
+	           --arg repo "$REPO" \
+	           --arg branch "$BRANCH" \
+		   '{"repo": $repo, "branch": $branch}')
+REQUEST_PAYLOAD=$(jq --null-input \
+	             --argjson repo_code "$CODE_PAYLOAD" \
+	             '{$repo_code}')
 
 API_ENDPOINT=https://api-staging.sqaaas.eosc-synergy.eu/v1
- 
+
 if [ -n "$BRANCH" ]; then
     BRANCH_MSG='using default branch'
 else
     BRANCH_MSG="branch: $BRANCH"
 fi 
-
 echo "Triggering SQAaaS assessment for repository $REPO ($BRANCH_MSG)"
-curl -X POST $API_ENDPOINT/pipeline/assessment --header '"Content-Type: application/json"' -d "$PAYLOAD"
+
+curl -X POST $API_ENDPOINT/pipeline/assessment --header '"Content-Type: application/json"' -d "$REQUEST_PAYLOAD"
 # --header "Authorization: Bearer ${TOKEN}"
 
 # echo "report={}" >> "$GITHUB_OUTPUT"
