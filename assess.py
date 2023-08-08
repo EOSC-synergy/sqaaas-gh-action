448192bc69fb44f656e70689edc3ffee86921571
Pablo Orviz <orviz@ifca.unican.es>
2023-08-08 12:59:03 +0200

    Report now includes badge results

--- assess.py
+++ assess.py
@@ -11,26 +11,48 @@ import sys
 import time
 
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('sqaaas-gh-action')
+
 COMPLETED_STATUS = ['SUCCESS', 'FAILURE', 'UNSTABLE', 'ABORTED']
 SUCCESFUL_STATUS = ['SUCCESS', 'UNSTABLE']
 # FIXME: add as CLI argument
 ENDPOINT = "https://api-staging.sqaaas.eosc-synergy.eu/v1"
-SUMMARY_TEMPLATE_TABLE= """
+BADGE_SHARE_MARKDOWN = {
+    'gold': {
+        'sqaaas': '[![SQAaaS badge](https://github.com/EOSC-synergy/SQAaaS/raw/master/badges/badges_150x116/badge_software_gold.png)]({assertion} "SQAaaS gold badge achieved")',
+        'shields': '[![SQAaaS badge shields.io](https://img.shields.io/badge/sqaaas%20software-gold-yellow)]({assertion} "SQAaaS gold badge achieved")'
+    },
+    'silver': {
+        'sqaaas': '[![SQAaaS badge](https://github.com/EOSC-synergy/SQAaaS/raw/master/badges/badges_150x116/badge_software_silver.png)]({assertion} "SQAaaS silver badge achieved")',
+        'shields': '[![SQAaaS badge shields.io](https://img.shields.io/badge/sqaaas%20software-silver-lightgrey)]({assertion} "SQAaaS silver badge achieved")'
+    },
+    'bronze': {
+        'sqaaas': '[![SQAaaS badge](https://github.com/EOSC-synergy/SQAaaS/raw/master/badges/badges_150x116/badge_software_bronze.png)]({assertion} "SQAaaS bronze badge achieved")',
+        'shields': '[![SQAaaS badge shields.io](https://img.shields.io/badge/sqaaas%20software-bronze-e6ae77)]({assertion} "SQAaaS bronze badge achieved")'
+    },
+}
+SUMMARY_TEMPLATE = """## SQAaaS results :bellhop_bell:
+
+### Quality criteria summary
+>>>>>>> 663874d (Report now includes badge results)
 | Result | Assertion | Subcriterion ID | Criterion ID |
 | ------ | --------- | --------------- | ------------ |
-{%- for result in results %}
+{%- for result in report_results %}
 | {{ ":heavy_check_mark:" if result.status else ":red_circle:" }} | {{ result.assertion }} | {{ result.subcriterion }} | {{ result.criterion }} |
-{%- endfor -%}
+{%- endfor %}
+
+### Quality badge
+{%- if badge_results.assertion %}
+ - SQAaaS-based badge: {{ badge_results.badge_sqaaas_md }}
+ - shields.io-based badge: {{ badge_results.badge_shields_md }}
+{%- else %}
+ - _No SQAaaS badge has been obtained_
+{%- endif %}
+ - Missing quality criteria for next level badge: __{{ badge_results.missing }}__
+
+### :clipboard: _View full report at_ ___{{ report_url }}___
 """
-SUMMARY_TEMPLATE = """## SQAaaS summary :bellhop_bell:
-
-{0}
-
-:clipboard: _View full report at_ ___{1}___
-"""
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('sqaaas-gh-action')
 
 
 def create_payload(repo, branch=None):
@@ -106,8 +128,6 @@ def run_assessment(repo, branch=None):
                 time.sleep(wait_period)
                 logger.info(f'Current status is {build_status}. Waiting {wait_period} seconds..')
         elif action in ['output']:
-            pipeline_id = '0c066a45-f2ab-46d7-b78f-308bb457ae82'
-            action = 'output'
             keep_trying = False
             response = sqaaas_request('get', f'pipeline/assessment/{pipeline_id}/{action}')
             sqaaas_report_json = response.json()
@@ -115,8 +135,9 @@ def run_assessment(repo, branch=None):
     return sqaaas_report_json
 
 
-def get_table(sqaaas_report_json):
-    assessment_results = []
+def get_summary(sqaaas_report_json):
+    # Collect quality report data
+    report_results = []
     for criterion, criterion_data in sqaaas_report_json['report'].items():
         for subcriterion, subcriterion_data in criterion_data['subcriteria'].items():
             for evidence in subcriterion_data['evidence']:
@@ -126,32 +147,63 @@ def get_table(sqaaas_report_json):
                 msg = msg.replace('>', '_')
                 msg = msg.replace('\n', '<br />')
                 # Generate entry for result
-                assessment_results.append({
+                report_results.append({
                     'status': evidence['valid'],
                     'assertion': msg,
                     'subcriterion': subcriterion,
                     'criterion': criterion
                 })
-    template = jinja2.Environment().from_string(SUMMARY_TEMPLATE_TABLE)
-    return template.render(results=assessment_results)
+    # Collect quality badge data
+    badge_software = sqaaas_report_json['badge']['software']
+    assertion = badge_software['data']['openBadgeId']
+    badge_sqaaas_md, badge_shields_md, missing = (None, None, None)
+    for badgeclass in ['gold', 'silver', 'bronze']:
+        missing = badge_software['criteria'][badgeclass]['missing']
+        badge_share_data = BADGE_SHARE_MARKDOWN[badgeclass]
+        if not missing:
+            badge_sqaaas_md = badge_share_data['sqaaas'].format(
+                assertion=assertion
+            )
+            badge_shields_md = badge_share_data['shields'].format(
+                assertion=assertion
+            )
+            break
+    badge_results = {
+        'assertion': assertion,
+        'badge_sqaaas_md': badge_sqaaas_md,
+        'badge_shields_md': badge_shields_md,
+        'missing': missing
+    }
+    # Render & return report
+    template = jinja2.Environment().from_string(SUMMARY_TEMPLATE)
+    return template.render(
+        report_results=report_results,
+        badge_results=badge_results,
+        report_url=sqaaas_report_json['meta']['report_json_url']
+    )
 
 
-def write_summary(*args):
-    if "GITHUB_STEP_SUMMARY" in os.environ :
+def write_summary(sqaaas_report_json):
+    summary = get_summary(sqaaas_report_json)
+    if "GITHUB_STEP_SUMMARY" in os.environ:
         logger.info('Setting GITHUB_STEP_SUMMARY environment variable')
         with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as f :
-            print(SUMMARY_TEMPLATE.format(*args), file=f)
+            print(summary, file=f)
+            logger.info('Summary data added to GITHUB_STEP_SUMMARY')
+    else:
+        logger.warning('Cannot set GITHUB_STEP_SUMMARY')
+
+    return summary
 
 
 def main(repo, branch=None):
-    # Get assessment report (JSON format)
-    sqaaas_report_json = run_assessment(repo, branch=None)
+    sqaaas_report_json = run_assessment(repo, branch=branch)
     if sqaaas_report_json:
-        table = get_table(sqaaas_report_json)
-        logger.info(table)
-        report_url = sqaaas_report_json['meta']['report_json_url']
-        logger.info(SUMMARY_TEMPLATE.format(table, report_url))
-        write_summary(table, report_url)
+        logger.info('SQAaaS assessment data obtained. Creating summary..')
+        logger.debug(sqaaas_report_json)
+        summary = write_summary(sqaaas_report_json)
+        if summary:
+            logger.debug(summary)
     else:
         logger.info('Could not get report data from SQAaaS platform')
 
